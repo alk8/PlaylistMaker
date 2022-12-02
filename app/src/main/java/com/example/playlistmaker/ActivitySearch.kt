54518c128f872412caccd454d897f70afd9ef2da
alk <l3pxbsk2kh>
2022-12-02 10:00:10 +0300

    added logic for search

--- app/src/main/java/com/example/playlistmaker/ActivitySearch.kt
+++ app/src/main/java/com/example/playlistmaker/ActivitySearch.kt
@@ -2,10 +2,51 @@ package com.example.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 
 class ActivitySearch : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val search = findViewById<EditText>(R.id.edit_search)
+        val clear = findViewById<Button>(R.id.clear_search)
+
+        val simpleTextWatcher = object :TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            visibleInvisibleClearButton(search,clear)
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+
+        }
+        search.addTextChangedListener(simpleTextWatcher)
+
+        clear.setOnClickListener {
+            search.text.clear()
+            visibleInvisibleClearButton(search,clear)
+        }
+
     }
+
+    private fun visibleInvisibleClearButton(search: EditText,clear: Button){
+
+        if (search.text.isEmpty()){
+            clear.visibility = View.INVISIBLE
+        }else{
+            clear.visibility = View.VISIBLE
+        }
+
+    }
+
 }
