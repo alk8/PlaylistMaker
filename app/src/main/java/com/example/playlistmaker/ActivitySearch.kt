1a39ca0017e00429cd448018796ec8eabb190bbd
alk <l3pxbsk2kh>
2022-12-07 13:55:01 +0300

    Исправлено согласно замечаниям. Добавлено скрытие клавиатуры

--- app/src/main/java/com/example/playlistmaker/ActivitySearch.kt
+++ app/src/main/java/com/example/playlistmaker/ActivitySearch.kt
@@ -1,16 +1,15 @@
 package com.example.playlistmaker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.text.Editable
-import android.text.InputType
 import android.text.TextWatcher
-import android.view.View
-import android.widget.Button
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
+
 
 class ActivitySearch : AppCompatActivity() {
 
@@ -47,7 +46,10 @@ class ActivitySearch : AppCompatActivity() {
 
         clear.setOnClickListener {
             search.text.clear()
-            search.setInputType(InputType.TYPE_NULL);
+            this.currentFocus?.let { view ->
+                val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+                imm?.hideSoftInputFromWindow(view.windowToken, 0)
+            }
             visibleInvisibleClearButton(search,clear)
         }
 
