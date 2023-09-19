a25ca5f93163b3a7a6fb13f2383223f37331725c
alk <l3pxbsk2kh>
2023-09-19 10:29:42 +0300

    Цвет карточек

--- app/src/main/java/com/example/playlistmaker/data/repository/SettingsRepositoryImpl.kt
+++ app/src/main/java/com/example/playlistmaker/data/repository/SettingsRepositoryImpl.kt
@@ -1,11 +1,9 @@
 package com.example.playlistmaker.data.repository
 
-import android.annotation.SuppressLint
 import android.content.SharedPreferences
 import com.example.playlistmaker.domain.api.SettingsRepository
 
 class SettingsRepositoryImpl(private val sharedPreferences: SharedPreferences):SettingsRepository {
-    @SuppressLint("CommitPrefEdits")
     override fun saveDarkThemeValue(value: Boolean) {
         sharedPreferences.edit().putBoolean("isDarkTheme",value).apply()
     }
